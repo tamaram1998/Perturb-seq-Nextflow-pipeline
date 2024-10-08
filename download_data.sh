055767d35e62b7af7ed3219f0aaf4bbe41c2c067
tamaram1998 <150626363+tamaram1998@users.noreply.github.com>
2024-10-08 22:55:25 +0200

    Added download data script for test files download

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+# Download genome test file
+wget https://pipeline-test-datasets.s3.amazonaws.com/test_datasets/genome_test.fa -O ./test_datasets/genome_test.fa
+
+# Download GTF test file
+wget https://pipeline-test-datasets.s3.amazonaws.com/test_datasets/gtf_test.gtf -O ./test_datasets/gtf_test.gtf
+
+# Download small FASTQ test file
+wget https://pipeline-test-datasets.s3.amazonaws.com/test_datasets/SRR23584814_small.fastq -O ./test_datasets/SRR23584814_small.fastq
+
